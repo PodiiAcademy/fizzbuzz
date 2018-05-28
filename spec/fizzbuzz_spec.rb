53cd94299294fcb2e92b2562a7bb0da62b28f072
alphonceakim <aliciakheem@gmail.com>
2018-05-28 15:51:33 +0300

    code that returns 3 if the number is divisible by 3 ,returns 5 if the number is divisible by 5 and returns any number that is neither divisible by 3 nor 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,5 +6,11 @@ describe 'fizzbuzz' do
    it 'returns "buzz" when passed 5' do
    	expect(fizzbuzz(5)).to eq 'buzz'
    end
-  end
+   it 'returns 1 when passed 1' do
+   	expect(fizzbuzz(1)).to eq 1
+   end
+   it 'returns 2 when passed 2' do
+   	expect(fizzbuzz(2)).to eq 2
+   end
+ end
    
